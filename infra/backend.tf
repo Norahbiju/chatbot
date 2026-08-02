bb26f4786d0384e52710d87441cd03400f9b5374
Norah Elizabeth Biju(UST <307435@ust.com>
2026-08-02 12:38:05 +0530

    chore: pin terraform s3 backend config

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket       = "chatbot-aws-tf-state-484632959006"
+    key          = "bedrock-rag/dev/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
